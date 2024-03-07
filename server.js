04a1a623fdbfd90fd5dc8f057c529b9472935912
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-03-07 10:15:24 -0500

    fixed error in category.js, removed commented code from the other files.

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Sync Sequelize models to the MySQL database
+sequelize.sync({ force: false }).then(() => {
+  console.log('Sequelize models synced to the database.');
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.error('Unable to sync Sequelize models:', err);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Handle 404 errors
+app.use((req, res) => {
+  res.status(404).send("Sorry can't find that!");
 });
